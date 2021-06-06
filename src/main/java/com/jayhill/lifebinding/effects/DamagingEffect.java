1841342f975eb03171d3d16c565e99907143da8a
jayhillx <69263647+jayhillx@users.noreply.github.com>
2021-06-06 01:50:18 -0400

    updated damaging effect

--- src/main/java/com/jayhill/lifebinding/effects/DamagingEffect.java
+++ src/main/java/com/jayhill/lifebinding/effects/DamagingEffect.java
@@ -3,6 +3,7 @@ package com.jayhill.lifebinding.effects;
 import com.jayhill.lifebinding.init.ModDamageSource;
 import com.jayhill.lifebinding.potions.LifeBindingPotion;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 
@@ -17,14 +18,16 @@ public class DamagingEffect extends Effect {
      * This effect will kill the player.
      */
     public void performEffect(LivingEntity entityLivingBaseIn, int amplifier) {
+        PlayerEntity player = (PlayerEntity)entityLivingBaseIn;
         if (this == LifeBindingPotion.LIFE_DAMAGING_EFFECT.get()) {
-            entityLivingBaseIn.attackEntityFrom(ModDamageSource.LIFE_BINDING, 1.5F);
+
+            entityLivingBaseIn.attackEntityFrom(ModDamageSource.LIFE_BINDING.setDamageBypassesArmor().setDamageAllowedInCreativeMode().setDamageIsAbsolute(), 1.0F);
         }
     }
 
     public boolean isReady(int duration, int amplifier) {
         if (this == LifeBindingPotion.LIFE_DAMAGING_EFFECT.get()) {
-            int i = 60 >> amplifier;
+            int i = 40 >> amplifier;
             if (i > 0) {
                 return duration % i == 0;
             } else {
