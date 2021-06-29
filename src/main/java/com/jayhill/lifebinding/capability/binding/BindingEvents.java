f5f279417acbbb45b0d41c63f5134076a6366e5d
jayhillx <69263647+jayhillx@users.noreply.github.com>
2021-06-29 06:16:13 -0400

    updated death & other events

--- src/main/java/com/jayhill/lifebinding/capability/binding/BindingEvents.java
+++ src/main/java/com/jayhill/lifebinding/capability/binding/BindingEvents.java
@@ -1,23 +1,28 @@
 package com.jayhill.lifebinding.capability.binding;
 
-import com.jayhill.lifebinding.capability.BoundPlayersList;
-import com.jayhill.lifebinding.init.ModDamageSource;
+import com.jayhill.lifebinding.effects.DoomedEffect;
+import com.jayhill.lifebinding.init.BoundDamageSource;
 import com.jayhill.lifebinding.potions.LifeBindingPotion;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.event.entity.living.PotionEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.living.PotionEvent.*;
+import net.minecraftforge.event.entity.player.PlayerEvent.*;
 import net.minecraftforge.event.entity.player.PlayerEvent.Clone;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 @SuppressWarnings("all")
 public class BindingEvents {
+
+    Set<UUID> players = new HashSet<>();
 
     @SubscribeEvent
     public void attachPlayerCapabilities(AttachCapabilitiesEvent<Entity> event) {
@@ -33,61 +38,62 @@ public class BindingEvents {
 
         if (entity instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity)entity;
-            for (ServerPlayerEntity serverPlayerEntity : player.getServer().getPlayerList().getPlayers()) {
 
-                IBoundCapability bound = player.getCapability(BindingCapabilities.LIFE_BOUND_CAPABILITY).orElse(null);
+            IBoundCapability bound = player.getCapability(BindingCapabilities.LIFE_BOUND_CAPABILITY).orElse(null);
 
-                /** Checks if the source is from our damaging effect. */
-                if (event.getSource() instanceof ModDamageSource) {
-                    serverPlayerEntity.removePotionEffect(LifeBindingPotion.LIFE_DAMAGING_EFFECT.get());
-                    BoundPlayersList.removeUsername(bound.getUUID());
-                }
+            if (bound.isBound()) {
+                /** Prevents player from dying if the event was caused by doomed effect. */
+                if (event.getSource() instanceof BoundDamageSource) {
+                    if (!(player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()) == null)) {
 
-                /** Checks if the bound player is offline.
-                 * If so, adds their name to the death list. */
-                if (player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()) == null) {
-                    BoundPlayersList.setUsername(bound.getUUID(), bound.getName());
-                } else if (!player.world.isRemote) {
-                    /** If the player is online, remove their name and give them the effect. */
-                    player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()).addPotionEffect(new EffectInstance(LifeBindingPotion.LIFE_DAMAGING_EFFECT.get(), 250));
-                    BoundPlayersList.removeUsername(bound.getUUID());
+                        player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()).removePotionEffect(LifeBindingPotion.DOOMED_EFFECT.get());
+                    }
+                    /** Removes players name if they died. */
+                    players.remove(player.getUniqueID());
+                } else {
+                    if (player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()) == null) {
+                        players.add(bound.getUUID());
+
+                    } else {
+                        player.getServer().getPlayerList().getPlayerByUUID(bound.getUUID()).addPotionEffect(new EffectInstance(LifeBindingPotion.DOOMED_EFFECT.get(), 300));
+                    }
                 }
             }
         }
     }
 
     @SubscribeEvent
-    public void onPotionExpire(PotionEvent.PotionExpiryEvent event) {
+    public void onPotionExpire(PotionExpiryEvent event) {
         Entity entity = event.getEntity();
 
         if (entity instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity)entity;
 
-            if (event.getPotionEffect().getPotion() == LifeBindingPotion.LIFE_DAMAGING_EFFECT.get()) {
-                ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)player;
-
-                IBoundCapability bound = player.getCapability(BindingCapabilities.LIFE_BOUND_CAPABILITY).orElse(null);
-
-                /** Drains the rest of the players health when the potion expires. */
-                player.attackEntityFrom(ModDamageSource.causeBoundPlayer(player).setDamageBypassesArmor().setDamageIsAbsolute(), player.getHealth());
-
-                serverPlayerEntity.removePotionEffect(LifeBindingPotion.LIFE_DAMAGING_EFFECT.get());
-                BoundPlayersList.removeUsername(bound.getUUID());
+            if (event.getPotionEffect().getPotion() == LifeBindingPotion.DOOMED_EFFECT.get()) {
+                /** Removes the rest of the players health when the potion expires. */
+                player.attackEntityFrom(BoundDamageSource.causeBoundPlayer(player).setDamageBypassesArmor().setDamageIsAbsolute().setDamageAllowedInCreativeMode(), player.getHealth());
             }
         }
     }
 
     @SubscribeEvent
-    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+    public void onPotionRemoved(PotionRemoveEvent event) {
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+
+        /** Prevents the player from removing the effect. */
+        if (event.getPotion() instanceof DoomedEffect) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerLogin(PlayerLoggedInEvent event) {
         PlayerEntity player = (PlayerEntity) event.getEntityLiving();
 
         if (player.getCapability(BindingCapabilities.LIFE_BOUND_CAPABILITY).orElse(null).isBound()) {
-
-            /** Checks if the players uuid who logged in, matches a uuid the death list. */
-            if (BoundPlayersList.containsUUID(player.getUniqueID())) {
-                /** If so, applies effect to the player. */
-                player.addPotionEffect(new EffectInstance(LifeBindingPotion.LIFE_DAMAGING_EFFECT.get(), 150));
-                BoundPlayersList.removeUsername(player.getUniqueID());
+            /** Checks if the players uuid who logged in, matches a uuid the hashset. */
+            if (players.contains(player.getUniqueID())) {
+                player.addPotionEffect(new EffectInstance(LifeBindingPotion.DOOMED_EFFECT.get(), 400));
             }
         }
     }
